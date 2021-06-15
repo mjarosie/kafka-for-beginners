63078e1449530a6b9e6726731f72117c3128aa48
mjarosie <mjarosie@users.noreply.github.com>
2021-06-15 23:47:07 +0100

    java consumer - consume messages and use assign and seek functionality

--- consumers/java-consumer/src/main/java/com/mjarosie/ConsumerDemoAssignSeek.java
+++ consumers/java-consumer/src/main/java/com/mjarosie/ConsumerDemoAssignSeek.java
@@ -1,45 +1,57 @@
 package com.mjarosie;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
 
-public class ConsumerDemo
+public class ConsumerDemoAssignSeek
 {
     public static void main( String[] args )
     {
-        final Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
+        final Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignSeek.class);
 
         final String bootstrapServers = "kafka:9092";
-        final String groupId = "my-fourth-application";
         final String topic = "first_topic";
 
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
-        consumer.subscribe(Collections.singleton(topic));
+        TopicPartition parititionToReadFrom = new TopicPartition(topic, 0);
+        long partitionOffset = 2L;
 
-        while(true) {
+        consumer.assign(Arrays.asList(parititionToReadFrom));
+        consumer.seek(parititionToReadFrom, partitionOffset);
+
+        int numberOfMessagesToRead = 5;
+        int numberOfMessagesReadSoFar = 0;
+        boolean keepOnReading = true;
+
+        while(keepOnReading) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
             for (ConsumerRecord<String, String> record : records) {
                 logger.info("key: " + record.key() + ", value: " + record.value());
                 logger.info("partition: " + record.partition() + ", offset: " + record.offset());
+                numberOfMessagesReadSoFar++;
+                if (numberOfMessagesReadSoFar > numberOfMessagesToRead) {
+                    keepOnReading = false;
+                    break;
+                }
             }
         }
     }
